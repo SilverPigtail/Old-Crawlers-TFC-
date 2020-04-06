7887727525a696d23748f32645266b1ebf959167
SilverPigtail <3512126@gmail.com>
2020-04-06 23:50:46 +0200

    V -> 0.1 Movement, hitboxes and tiledmap.
    -> Created the Unity project.
    -> Added the character sprite and movement script file (C#).
    -> Added isometric tiledmap (created in the Unity's tilemap editor).
    -> Added map hitboxes (with collisions).

---
+++ OldCrawlers_TFC/Assets/Scripts/HideColliderColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class HideColliderColor : MonoBehaviour
+{
+    private TilemapRenderer tilemapRenderer;
+
+    private void Awake()
+    {
+        tilemapRenderer = GetComponent<TilemapRenderer>();
+        
+    }
+
+    private void Start()
+    {
+        tilemapRenderer.enabled = false;
+    }
+}
---
+++ OldCrawlers_TFC/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    [SerializeField] private float moveSpeed = 2.0f;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveH = Input.GetAxis("Horizontal") * moveSpeed;
+        moveV = Input.GetAxis("Vertical") * moveSpeed;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveH, moveV);
+    }
+}
