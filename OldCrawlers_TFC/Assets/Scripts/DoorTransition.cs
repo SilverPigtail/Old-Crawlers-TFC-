64e8e864606353e02daf699760125b1de53ceb0d
SilverPigtail <3512126@gmail.com>
2020-04-13 18:12:27 +0200

    V -> 0.5 Random Scene generation.
    - Added doors that allows the user to change between scene using random values.
    
    - Solved Palette problems.
    
    - Created another two maps.

---
+++ OldCrawlers_TFC/Assets/Scripts/DoorTransition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = System.Random;
+
+public class DoorTransition : MonoBehaviour
+{
+    public String sceneToLoad;
+    private int mapGenerator;
+
+    
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        Random rnd = new Random();
+        mapGenerator = rnd.Next(0, 3);
+            
+            if (other.CompareTag("Player") && !other.isTrigger && mapGenerator == 1)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+
+            if (other.CompareTag("Player") && !other.isTrigger && mapGenerator == 2)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+            }
+
+            if (other.CompareTag("Player") && !other.isTrigger && mapGenerator == 3)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+            }
+    }
+}
