46b67d4410c7a582a36c3baa17b11dfcb4e01e63
SilverPigtail <3512126@gmail.com>
2020-05-03 21:56:55 +0200

    V -> 0.8.0.1 Started Documentation
    - Added enemies AI class a descritpion of its functions.
    
    - Some parameters get their names changed.
    
    - Deleted thrash script.

--- OldCrawlers_TFC/Assets/Scripts/EnemyMovementIA.cs
+++ OldCrawlers_TFC/Assets/Scripts/EnemyMovementIA.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+/***
+ * This class models the enemy AI, allowing the enemies to follow the target (in this case the player) anywhere he goes.
+ */
+
+public class EnemyMovementIA : MonoBehaviour
 {
     [SerializeField] private float maxEnemyDistance;
 
@@ -14,14 +18,22 @@ public class EnemyController : MonoBehaviour
     private Transform targetFollow;
     public Transform respawnPosition;
     
-    // Start is called before the first frame update
+    
+    /***
+     * This funcion is called before the first frame updtates. Here we get the Animator of the enemy that is created in
+     * the Unity's editor and we said to the editor who is the target. In this case, it is the character's sprite.
+     */
     void Start()
     {
         enemyAnim = GetComponent<Animator>();
         targetFollow = FindObjectOfType<PlayerMovement>().transform;
     }
-
-    // Update is called once per frame
+    
+    /***
+     * This function is called one time per frame. Here we check the distance that the character's sprite is. If the
+     * distance is in the limits that we established in the Unity's editor, the enemy will follow the target. If not,
+     * the enemy will return to his respawn position.
+     */
     void Update()
     {
         if (Vector3.Distance(targetFollow.position, transform.position) <= maxEnemyDistance && Vector3.Distance(targetFollow.position, transform.position) >= minEnemyDistance)
@@ -34,6 +46,12 @@ public class EnemyController : MonoBehaviour
         }
        
     }
+    
+    /***
+     * This function is used to return the enemy to his respawn point. We transform the position of the current coords,
+     * the player's sprite distance and the enemy speed in position, if that conditions are true, we set the boolean of
+     * the Unity's animations editor parameter to false, and the enemy will return tho his respawn point.
+     */
 
     public void RespawnEnemy()
     {
@@ -48,6 +66,10 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    /***
+     * This function allows the enemy to follow the Character's sprite arount the tilemap. Here we set the players coords
+     * of the tilemap in the Unity's animations editor, and then we track the sprite.
+     */
     public void FollowPlayer()
     {
         enemyAnim.SetBool("isMoving", true);
