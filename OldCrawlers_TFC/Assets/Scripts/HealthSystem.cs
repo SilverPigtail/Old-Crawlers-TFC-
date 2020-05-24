418ab1558d1c839f4bc2e79b837fb51cc6281840
SilverPigtail <3512126@gmail.com>
2020-05-24 13:47:36 +0200

    V -> 0.8.5 Enemies interactions
    - The enemies can now hit the player, when this happens, the health points of the character will get down.
    
    - Scripts added: "HurtCharacter", "HealthSystem".

---
+++ OldCrawlers_TFC/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int currentHP;
+    public int maxHP;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HurtCharacter(int damageTaken)
+    {
+        currentHP -= damageTaken;
+
+        if (currentHP <= 0)
+        {
+            gameObject.SetActive(false);
+            
+        }
+        
+    }
+}
---
+++ OldCrawlers_TFC/Assets/Scripts/HurtCharacter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HurtCharacter : MonoBehaviour
+{
+    private float timeWaited = 2f;
+    private bool rel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      /*  if (rel)
+        {
+            timeWaited -= Time.deltaTime;
+            if (timeWaited <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        } */
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player") // You can use <<other.collider.compareTag("Player")>> to make this condition.
+        {
+            //other.gameObject.SetActive(false);
+            other.gameObject.GetComponent<HealthSystem>().HurtCharacter(10);
+            //rel = true;
+        }
+    }
+
+
+}
