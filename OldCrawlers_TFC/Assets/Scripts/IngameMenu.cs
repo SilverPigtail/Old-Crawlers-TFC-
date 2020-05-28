b554b1c923448ad015764db989a8ed61491a0efe
SilverPigtail <3512126@gmail.com>
2020-05-29 01:26:58 +0200

    V -> 0.9.3 Ingame Menu
    - Semi functional ingame menu that the player can make appear just pressing the escape key, this works thanks to the "IngameMenu" Script.
    
    - Added IngameMenu Script.

---
+++ OldCrawlers_TFC/Assets/Scripts/IngameMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public GameObject pauseInterface;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        pauseInterface.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    void ResumeGame()
+    {
+        pauseInterface.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
