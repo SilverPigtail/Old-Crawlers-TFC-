e75a0da191895deae23718d4f8f21495d03a127c
SilverPigtail <3512126@gmail.com>
2020-05-28 20:31:58 +0200

    V -> 0.9 Room marker
    - Added script that allows the stage text to get the current room that the main character is in.
    
    - Changed some enemies positions and added to other levels.
    
    - Added a TextMesh's interface that get the name of the current room with te script "StageText".

---
+++ OldCrawlers_TFC/Assets/Scripts/StageText.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class StageText : MonoBehaviour
+{
+    private string currentStage;
+    private Scene currentScene;
+    private TextMeshProUGUI textMeSh;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMeSh = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            textMeSh.text = "Room 1";
+        }
+
+        if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            textMeSh.text = "Room 2";
+        }
+
+        if (SceneManager.GetActiveScene().buildIndex == 3)
+        {
+            textMeSh.text = "Room 3";
+        }
+
+        if (SceneManager.GetActiveScene().buildIndex == 4)
+        {
+            textMeSh.text = "Room 4";
+        }
+
+        if (SceneManager.GetActiveScene().buildIndex == 5)
+        {
+            textMeSh.text = "Room 5";
+        }
+        
+       
+    }
+}
